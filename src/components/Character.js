70dc5e77745abdf35db61a188f69be42ab165323
BelaM80 <mueller.bela@gmx.de>
2020-10-21 11:54:29 +0200

    Create CHaracter.js

---
+++ src/components/Character.js
@@ -0,0 +1,21 @@
+import { container } from "webpack";
+import { createElement } from "../utils/elements";
+
+function Character({ name, imgSrc }) {
+  const title = createElement("div", {
+    innerText: name,
+  });
+  const avatar = createElement("img", {
+    src: imgSrc,
+    alt: name,
+  });
+
+  const conatainer = createElement("div", {
+    clasName: "character",
+    children: [title, avatar],
+  });
+
+  return container;
+}
+
+export default Character;
